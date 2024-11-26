3677661b557c3b3fe7bb3e91c5efc6b5da06766a
Your Name <k.bristi057@gmail.com>
2024-11-26 18:57:58 +0600

    added the updated part for post method

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+app.use(express.json());
 
 const users = [
 
@@ -23,6 +24,15 @@ app.get('/users', (req,res)=>{
 app.get('/', (req, res) => {
     res.send('Users management server running')
 
+})
+
+app.post('/users', (req,res)=>{
+    console.log('post api hitting')
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser)
 })
 
 app.listen(port, () => {
